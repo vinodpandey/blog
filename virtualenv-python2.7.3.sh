082d92ef874c31cf4021d7a2ca1038940dd6114d
Vinod Pandey <vpforyou@gmail.com>
2013-04-03 00:01:31 -0700

    Update virtualenv-python2.7.3.sh

--- virtualenv-python2.7.3.sh
+++ virtualenv-python2.7.3.sh
@@ -11,43 +11,61 @@
 # deactivate
 # test will now have virtual environment created with Python 2.7.3
 
-
+# check python version, if version 2.7.3 not present, install it
 if [ "$(python2.7 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')" != "2.7.3" ]; then
-    if [ "$SUDO_USER" = "" ]; then
-        echo "Please run this script with sudo. We need to install Python 2.7.3"
-    else
-        echo "Installing Python 2.7.3"
-        sudo yum -y install zlib zlib-devel gcc httpd-devel
-        mkdir -p temp
-        cd temp
-        wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
-        tar zxvf Python-2.7.3.tgz
-        cd Python-2.7.3
-        ./configure --prefix=/usr/local --with-threads --enable-shared --with-zlib=/usr/include
-        make
-        sudo make altinstall
-        cd ..
-        cd ..
-        rm -rf temp
-        sudo echo "/usr/local/lib" > /etc/ld.so.conf.d/python2.7.conf 
-        sudo /sbin/ldconfig
-        sudo ln -s /usr/local/bin/python2.7 /usr/bin/python2.7
-    fi
-
+    echo "Installing Python 2.7.3"
+    sudo yum -y install zlib zlib-devel gcc httpd-devel
+    mkdir -p temp
+    cd temp
+    wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
+    tar zxvf Python-2.7.3.tgz
+    cd Python-2.7.3
+    ./configure --prefix=/usr/local --with-threads --enable-shared --with-zlib=/usr/include
+    make
+    sudo make altinstall
+    cd ..
+    cd ..
+    rm -rf temp
+    sudo echo "/usr/local/lib" > python2.7.conf 
+    sudo mv python2.7.conf /etc/ld.so.conf.d/python2.7.conf 
+    sudo /sbin/ldconfig
+    sudo ln -s /usr/local/bin/python2.7 /usr/bin/python2.7
 else
-    echo "Creating virtualenv"
-    wget http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.5.1.tar.gz
-    tar zxvf virtualenv-1.5.1.tar.gz
-    python2.7 virtualenv-1.5.1/virtualenv.py --no-site-packages .
-    source bin/activate
-    python -V 
-    pip install fabric
-    rm -rf virtualenv-1.5.1  virtualenv-1.5.1.tar.gz
-    deactivate
+   echo "Python 2.7.3 present"
+fi
+# check easy_install, if not present, install it
+if [[ $(easy_install-2.7 --version) != distribute* ]]; then
+    echo "Installing easy_install-2.7"
+    wget http://pypi.python.org/packages/source/d/distribute/distribute-0.6.35.tar.gz
+    tar zxvf distribute-0.6.35.tar.gz
+    cd distribute-0.6.35
+    sudo python2.7 setup.py install
+    sudo ln -s /usr/local/bin/easy_install-2.7 /usr/bin/easy_install-2.7
+    cd ..
+    rm -rf distribute-0.6.35.tar.gz distribute-0.6.35
+else
+    echo "easy_install-2.7 present"
+fi
+  
+# check pip, if not present, install it
+if [[ $(pip-2.7 --version) != pip* ]]; then
+   echo "Installing pip" 
+   sudo easy_install-2.7 pip
+else
+   echo "pip-2.7 present"
 fi
 
+# check if virtualenv is present, otherwise install it
+if [[ $(virtualenv-2.7 --version) != 1.9.1* ]]; then
+    sudo pip-2.7 install virtualenv==1.9.1
+else
+    echo "virtualenv-2.7 v1.9.1 present"
+fi
 
- 
-
-
-
+# finally, create the virtual environment
+echo "Creating virtual environment"
+virtualenv-2.7 --no-site-packages .
+source bin/activate
+python -V 
+pip install fabric
+deactivate
